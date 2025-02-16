7af415569bdb2c54264d1fbf4428ea8c603f7c76
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2025-02-16 08:35:17 -0300

    start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@
 
 const Header = () => {
     return (
-        <div className="bg-[#1d2125]">
-                Header
+        <div className="bg-[#1d2125] w-100vw h-12 p-3 border-b bordered-box flex flex-row justify-between border-b-[#9fadbc29]">
+                <div className="left">
+                    <h3>Trello Clone</h3>
+                </div>
+
+                <div className="rigth flex items-center space-x-4">
+                    <span>Remote Dev</span>
+                    <img src="https://placeholder.co/28x28/png" alt="" />
+                </div>
         </div>
     )
 }

dfb2698451472e46ce2b652f2c478cf46aeebd5a
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2025-02-22 12:22:58 -0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,114 +1,48 @@
-//import React from "react";
-import { useState, useContext, Children } from "react";
-import { ChevronRight, ChevronLeft, Plus, X,  } from "react-feather";
-import { Popover } from "react-tiny-popover";
-import { BoardContext } from "../context/BoardContext";
-import { createContext } from "react";
-
-export const BoardContext = createContext();
-
-export const BoardProvider = ({ children }) =>  {
-  const [allboard, setAllBoard] = useState({
-    boards: [{ name: "Board 1", bgcolor: "#ff5733"}],
-  })
-
-
-return (
-  <BoardContext.Provider value={{ allboard, setAllBoard }}>  
-      {children}
-    </BoardContext.Provider>
-);
-
-};
-
+import { useState } from "react";
+import { ChevronLeft, ChevronRight, Plus } from "react-feather"
 
 const Sidebar = () => {
-  const [collapsed, setCollapsed] = useState(false);
-  const [showpop, setShowpop] = useState(false);
-  const [allboard, setAllBoard] = useContext(BoardContext);
 
+  const [collapsed,setCollapsed] = useState(false);
   return (
-    <div
-      className={`bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] transition-all linear duration-500 flex-shrink-0 ${
-        collapsed ? "w-[40px]" : "w-[280px]"
-      }`}
-    >
-      {collapsed && (
-        <div className="p-2">
-          <button
-            onClick={() => setCollapsed(!collapsed)}
-            className=" hover:bg-slate-600 rounded-sm"
-          >
+    <div className={`bg-[#121417] h-[calc(100vh-3rem)] border-r border-r-[#9fadbc29] transition-all linear duration-500 flex-shrink-0 ${collapsed ? 'w-[42px]' : 'w-[280px]'} `}>
+        
+        {collapsed && <div className="p-2">
+          <button onClick={()=> setCollapsed(!collapsed)} className="hover:bg-slate-600 rounded-sm p-1">
             <ChevronRight size={18}></ChevronRight>
           </button>
-        </div>
-      )}
+        </div>}
 
-      {!collapsed && (
-
-        <div>
-          
-          <div className="workspace p-3 flex justify-between border-b border-b-[#9fadbc29]">
+        {!collapsed && <div>
+            <div className="workspace p-3 flex justify-between border-b border-b-[#9fadbc29]">
             <h4>Remote Devs Workspace</h4>
-            <button
-              onClick={() => setCollapsed(!collapsed)}
-              className=" hover:bg-slate-600 p-1 rounded-sm"
-            >
+            <button onClick={()=> setCollapsed(!collapsed)} className="hover:bg-slate-600 rounded-sm p-1">
               <ChevronLeft size={18}></ChevronLeft>
             </button>
-          </div>
-
-          <div className="boardlist">
-            <div className="flex justify-between px-3 py-2">
-              <h6>Your Boards</h6>
-              
-              <Popover
-                isOpen={showpop}
-                align="start"
-                positions={["right","top", "bottom", "left"]} // preferred positions by priority
-                content={
-                      <div className="ml-2 p-2 w-60 flex flex-col justify-center items-center bg-slate-600 text-white rounded">
-                        <button onClick={() => setShowpop(!showpop)} className="absolute right-2 top-2 hover:bg-gray-500 p-1 rounded"><X size={16}></X></button>
-                        <h4 className=" p-3">Create board</h4>
-                        <img src="https://placehold.co/200x120/png" alt="" />
-                        <div className="mt-3 flex flex-col items-start w-full">
-                            <label htmlFor="title">Board Title <span>*</span></label>
-                            <input type="text" className="mb-2 h-8 px-2 w-full bg-gray-700" />
-                            <label htmlFor="Color">Board Color</label>
-                            <input type="color" className="mb-2 h-8 px-2 w-full bg-gray-700" />
-                            <button className="w-full rounded h-8 bg-slate-700 mt-2 hover:bg-gray-500">Create</button>
-                        </div>
-                      </div>
-                      }
-              >
-                <button onClick={() => setShowpop(!showpop)} className=" hover:bg-slate-600 p-1 rounded-sm">
-                <Plus size={16}></Plus>
-              </button>
-              </Popover>
-
             </div>
-          </div>
 
-          <ul>
-            {allboard.boards && allboard.boards.map((x, index)=>(
-                <li key={index}>
-              <button onClick={()=> setAllBoard} className="px-3 py-2 w-full text-sm flex justify-start align-baseline hover:bg-gray-700">
-                <span className="w-6 h-max rounded-sm mr-2 " style={{backgroundColor:`${x.bgcolor}`}}>
-                  &nbsp;
-                </span>
-                <span>{x.name}</span>
-              </button>
-            </li>
-            ))}
-              
-          </ul>
-        </div>
-      )}
-      
+            <div className="boardlist">
+                <div className="flex justify-between px-3 py-2">
+                    <h6>Your Boards</h6>
+                    <button className="hover:bg-slate-600 rounded-sm p-1">
+                      <Plus size={16}></Plus>
+                    </button>
+                </div>
+            </div>
+
+            <ul>
+              <li>
+                  <button className="px-3 py-2 w-full text-sm flex justify-start align-baseline hover:bg-gray-600">
+                      <span className="w-6 h-max rounded-sm mr-2 bg-red-600">&nbsp;</span>
+                      <span className=" flex justify-start">My Trello Board</span>
+                  </button>
+              </li>
+            </ul>
+
+        </div>}
+        
     </div>
   );
-};
+}
 
-export default Sidebar;
-
-
+export default Sidebar

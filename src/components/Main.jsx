ccd5a97c79a7b6150e4d3fd62d695b77d0abe63e
Alex Feitoza <159736255+AlexFeitoza@users.noreply.github.com>
2025-02-23 11:44:40 -0300

    comited

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,23 @@
 //import React from "react";
 import { Edit2, MoreHorizontal, UserPlus } from "react-feather";
 import CardAdd from "./CardAdd";
+import { BoardContext } from "../context/BoardContext";
+import { useContext } from "react";
 
 const Main = () => {
+
+const {allboard,setAllBoard} = useContext(BoardContext);
+const bgdata = allboard.boards[allboard.active];
+
+const cardData = (e) => {
+    let newList = [...bgdata.list];
+    newList
+}
+
   return (
     <div className="flex flex-col bg-slate-900 w-full">
       <div className="p-3 bg-black flex justify-between w-full bg-opacity-50">
-        <h2 className="text-lg">My Trello Board</h2>
+        <h2 className="text-lg">{bdata.name}</h2>
 
         <div className="flex items-center justify-center ">
           <button className="bg-gray-200 h-8 text-gray-500 px-2 py-1 mr-2 rounded flex justify-center items-center">
@@ -21,25 +32,31 @@ const Main = () => {
 
       <div className="flex flex-col w-full flex-grow relative">
         <div className="absolute mb-1 pb-2 left-0 right-0 top-0 bottom-0 p-3 flex overflow-x-scroll overflow-y-hidden ">
-          <div className="mr-3 w-60 h-fit rounded-md p-2 bg-black flex-shrink-0">
-            <div className="list-body">
-              <div className="flex justify-between p-1 ">
-                <span>To do</span>
-                <button className="hover:bg-gray-500 p-1 rounded-sm ">
-                  <MoreHorizontal size={16}></MoreHorizontal>
-                </button>
-              </div>
-
-              <div className="item flex justify-between items-center bg-zinc-700 p-1 rounded-md cursor-pointer border-2 border-zinc-900 hover:border-gray-500">
-                <span>Descrição do Projeto</span>
-                <span className="flex justify-self-start items-start">
-                    <button className="hover:bg-gray-600 p-1 rounded-sm"><Edit2 size={16}></Edit2></button>
-                </span>
-              </div>
-              <CardAdd></CardAdd>
-
-            </div>
-          </div>
+          {bdata.list && bdata.list.map((x,ind)=>{
+              return <div className="mr-3 w-60 h-fit rounded-md p-2 bg-black flex-shrink-0">
+               <div className="list-body">
+                 <div className="flex justify-between p-1 ">
+                   <span>{x.title}</span>
+                   <button className="hover:bg-gray-500 p-1 rounded-sm ">
+                     <MoreHorizontal size={16}></MoreHorizontal>
+                   </button>
+                 </div>
+   
+                {x.items && x.items.map((item,index)=>{
+                   return <div className="item flex justify-between items-center bg-zinc-700 p-1 rounded-md cursor-pointer border-2 border-zinc-900 hover:border-gray-500">
+                      <span>{item.title}</span>
+                      <span className="flex justify-self-start items-start">
+                          <button className="hover:bg-gray-600 p-1 rounded-sm"><Edit2 size={16}></Edit2></button>
+                      </span>
+                   </div>
+                })}
+                 <CardAdd getcard={(e)=>cardData(e)}></CardAdd>
+   
+               </div>
+             </div>
+          })
+          }
+         
         </div>
       </div>
     </div>
